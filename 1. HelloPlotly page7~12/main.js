e7cb61f6a8d4d0eb60296ee94523f0146d79620b
oldmoon <oldmoon945@gmail.com>
2024-06-07 11:00:36 +0800

    feat: HelloPlotly 新增註解
    feat: HelloPlotly 新增註解

--- 1. HelloPlotly page7~12/main.js
+++ 1. HelloPlotly page7~12/main.js
@@ -1,7 +1,10 @@
+// 取得 index.html id 命名為 ‘myGraph’ div區塊, 並將結果放在 myGraph 變數中
 let myGraph = document.getElementById('myGraph')
+
+// 使用 Plotly 在 id 命名為 ‘myGraph’ div區塊, 新增折線圖, 上面周遭邊界設為 50
 Plotly.newPlot(myGraph, [{
     x: [1, 2, 3, 4, 5],
     y: [1, 2, 4, 8, 16]
 }], {
-    margin: {t: 0,}
+    margin: 50,
 });
